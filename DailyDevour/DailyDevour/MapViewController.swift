e3e32980a74e4dfab9810d7e7022b7e5a0db1841
Ido Tamir <idotamir516@gmail.com>
2017-11-11 18:37:06 -0500

    I love to commit

---
+++ DailyDevour/DailyDevour/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  DailyDevour
+//
+//  Created by Ido Tamir on 11/11/17.
+//  Copyright © 2017 DailyDevour. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MapViewController: UIViewController {
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
+    
+}
